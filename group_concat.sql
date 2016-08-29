d8f721179b77d77fced39d81106e0eddf74c05cb
P.M Naing <phonemyintnaing@users.noreply.github.com>
2016-08-29 14:45:25 +0800

    Create group_concat.sql

---
+++ group_concat.sql
@@ -0,0 +1,16 @@
+--example: similar to 'group-concat' function 
+
+
+SELECT
+      m.maskid
+    , m.maskname
+    , m.schoolid
+    , s.schoolname
+    , maskdetail = STUFF((
+          SELECT ',' + md.maskdetail
+          FROM dbo.maskdetails md
+          WHERE m.maskid = md.maskid
+          FOR XML PATH(''), TYPE).value('.', 'NVARCHAR(MAX)'), 1, 1, '')
+FROM dbo.tblmask m
+JOIN dbo.school s ON s.ID = m.schoolid
+ORDER BY m.maskname
